b9c3f750c0614994ad4fae0bbed02ca569217214
Akpojiyovwi Tega <btega@quidax.com>
2025-05-02 14:29:31 +0100

    Create table

--- app.py
+++ app.py
@@ -74,5 +74,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    create_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
